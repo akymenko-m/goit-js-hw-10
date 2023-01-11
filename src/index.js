8439f05465a33b1895b45f39651c27694a1b2987
Mariia Akymenko <dafnarianna@gmail.com>
2023-01-11 15:55:11 +0200

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import './css/styles.css';
 
+const input = document.querySelector('#search-box');
+
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener('input', getInput);
+function getInput(event) {
+    console.log(event.currentTarget.value);
+    nameCountry = event.currentTarget.value;
+    
+    fetchCountries(nameCountry);
+    
+}
+
+
+function fetchCountries(name) {
+        // name = nameCountry;
+        fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`).then(response => {
+        // console.log(response.json());
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            
+            return response.json();
+        }).then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.log(error);
+        });
+}
+// fetchCountries();
+
+
+// fetch("https://restcountries.com/v3.1/name/peru?fields=name.official,capital,population,flags.svg,languages").then(response => {
+//     // console.log(response);
+//     if (!response.ok) {
+//         throw new Error(response.status);
+//     }
+
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+// fetch("https://restcountries.com/v2/name/peru?fields=name.official,capital,population,flags.svg,languages").then(response => {
+//     // console.log(response.json());
+//     return response.json();
+// }).then(country => {
+//     console.log(country);
+// }).catch(error => {
+//     console.log(error);
+// });
